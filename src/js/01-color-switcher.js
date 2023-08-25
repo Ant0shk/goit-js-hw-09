9e9407e11d4e1f2d41bfae1b191d0728806b1279
AntoninaYatsenko <dp130688jaj@gmail.com>
2023-08-25 12:20:28 +0100

    push

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,7 @@
 const startElement = document.querySelector('[data-start]');
 const stopElement = document.querySelector('[data-stop]');
 const bodyElement = document.body;
-
+let inter;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
